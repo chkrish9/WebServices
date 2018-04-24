782f74c0dadf5960f9e47792d72e9807c258396f
Murali <murali.24.4.91@gmail.com>
2018-04-24 22:33:06 +0530

    Added body-parser

--- Node/app.js
+++ Node/app.js
@@ -1,11 +1,26 @@
 const express =  require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productApi = require('./api/routes/products');
 const orderApi = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//    res.header('Access-Control-Allow-Origin', '*');
+//    res.header('Access-Control-Allow-Headers',
+//    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//    );
+//    if(req.method === 'OPTIONS')
+//    {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE');
+//         return res.status(200).json({})
+//    }
+// });
 
 app.use('/products', productApi);
 app.use('/orders', orderApi);
