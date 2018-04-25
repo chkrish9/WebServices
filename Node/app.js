d0483505ab9c71d5de473866c3d3c3247195e265
Murali <murali.24.4.91@gmail.com>
2018-04-25 23:39:04 +0530

    CORS added

--- Node/app.js
+++ Node/app.js
@@ -10,17 +10,18 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//    res.header('Access-Control-Allow-Origin', '*');
-//    res.header('Access-Control-Allow-Headers',
-//    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//    );
-//    if(req.method === 'OPTIONS')
-//    {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE');
-//         return res.status(200).json({})
-//    }
-// });
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers',
+   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+   );
+   if(req.method === 'OPTIONS')
+   {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE');
+        return res.status(200).json({})
+   }
+   next();
+});
 
 app.use('/products', productApi);
 app.use('/orders', orderApi);
