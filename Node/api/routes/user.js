3ca677c7f211cf495333e2e585221956c69a670e
Murali <murali.24.4.91@gmail.com>
2018-04-29 03:00:53 +0530

    added userAuth for user delete

--- Node/api/routes/user.js
+++ Node/api/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
+const userAuth = require('../middleware/check-auth');
 
 router.post('/signup', userController.signup);
 
 router.post('/login', userController.login);
 
-router.delete('/:id', userController.delete_user);
+router.delete('/:id', userAuth, userController.delete_user);
 
 module.exports = router;
